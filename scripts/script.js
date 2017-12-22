4cc221c8eb942208ddda31bf40b9a5c0e8b18a70
Tanya <tanya.murrow@hobbylobby.com>
2017-12-22 14:13:17 -0600

    Skills

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,28 @@
 
 $(document).ready(function() {
 
+  /* Skills table script
+  -------------------------------------------------- */
+  $('.custom-control-input').change(function() {
+    var skill = $(this).data('skill');
+    if ($(this).prop('checked'))
+    {
+      $('#' + skill + '-prof-bonus').removeClass('invisible');
+      var mod = parseInt($('#' + skill + '-mod').val());
+      $('#' + skill + '-mod').val(mod + 2);
+    }
+    else {
+      $('#' + skill + '-prof-bonus').addClass('invisible');
+      var mod = parseInt($('#' + skill + '-mod').val());
+      $('#' + skill + '-mod').val(mod - 2);
+    }
+  });
+
+  $('.skill-label').click(function() {
+    var skill = $(this).data('skill');
+    $('input[data-skill=' + skill + ']').click();
+  });
+
   /* Alignment button grid script
   -------------------------------------------------- */
   $('.alignment-grid button').click(function(e){
